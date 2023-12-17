21687abf31935a715b47b1e92597bdd1174a83c8
Colin <colin.diesh@gmail.com>
2023-12-16 22:30:18 -0500

    Updates

---
+++ _next/static/chunks/app_wormbase_styles_css_046983._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_wormbase_styles_css_046983._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_wormbase_styles_css_046983._.js",
+  "chunks": [
+    "static/chunks/app_wormbase_styles_619958.css"
+  ],
+  "source": "dynamic"
+});
